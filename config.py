0cdcbe70fa35d932dd536ca04edd1a9731fdfd5b
anx6ty <anx6ty@gmail.com>
2026-08-29 21:55:19 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+import os
 
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD_ID = int(os.getenv("GUILD_ID", "0"))
+ALERT_CHANNEL_ID = int(os.getenv("ALERT_CHANNEL_ID", "0"))
+
+# Heuristic weights (unchanged)
+WEIGHTS = {
+    "username_similarity": 5,
+    "same_avatar": 10,
+    "creation_close": 5,
+    "join_close": 5,
+    "same_discriminator": 2,
+}
+
+ALT_THRESHOLD = 8
+USERNAME_SIMILARITY_RATIO = 0.7
